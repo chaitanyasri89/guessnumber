739e4f8d9044992d514477560ba9130b28b14ae3
chaitanyasri89 <127734233+chaitanyasri89@users.noreply.github.com>
2023-06-22 16:28:18 +0530

    Add files via upload

---
+++ no.js
@@ -0,0 +1,24 @@
+.hi{
+    height: 350px;
+    width: 500px;
+    background-color: rgba(157, 217, 225, 0.54);
+    border: 4px solid black;
+    margin: auto;
+    margin-top: 160px;
+    box-shadow: 7px 3px 3px white;
+    transition: 1s ease;
+}
+.hi:hover{
+    scale: 1.2;
+
+}
+.r{
+    height: 20px;
+    width: fit-content;
+    border: 2px solid black;
+}
+.si{
+    height: 20pxpx;
+    width: 55px;
+    border: 2px solid black;
+}
